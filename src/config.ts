0700df4c803c7236d9a33f4ad9dd3f059048e3fd
Amit Burnwal <amitkumar060705@gmail.com>
2026-04-22 21:37:47 +0530

    making ready for production

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const JWT_SECRET="!123123";
-export const mongoUrl="mongodb+srv://amitkumar060705_db_user:mdYpQRX5fUYKri53@cluster0.vgnuusl.mongodb.net/"
+export const JWT_SECRET=process.env.JWT_SECRET || "!123123";
+export const mongoUrl=process.env.MONGO_URL || "mongodb+srv://amitkumar060705_db_user:mdYpQRX5fUYKri53@cluster0.vgnuusl.mongodb.net/"
--- src/index.ts
+++ src/index.ts
@@ -115,4 +115,7 @@ app.get("/api/v1/brain/:shareLink", async (req, res) => {
         content: content
     })
 })
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
